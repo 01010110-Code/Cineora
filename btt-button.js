9bc613d591ead40ecbd928b51938288468e85980
Bahaa Tantaoui <tantaouibahaabusiness@gmail.com>
2023-03-08 22:44:29 +0100

    added a simple go back to top button, not styled yet

---
+++ btt-button.js
@@ -0,0 +1,20 @@
+window.onscroll = function() {
+    scrollFunction()
+  };
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("scroll-to-top").style.display = "block";
+    } else {
+      document.getElementById("scroll-to-top").style.display = "none";
+    }
+  }
+  document.getElementById("scroll-to-top").onclick = function() {
+    scrollToTop()
+  };
+  
+  function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+  
